2bb1793b12eaffd4326c82c2870eb3cdb958bbaf
Owner <Owner@DESKTOP-RF4V7UB>
2019-07-16 19:16:12 +0530

    Second commit

---
+++ Seleniumdemo1/src/demoselenium1/Webdriverdemo.java
@@ -0,0 +1,17 @@
+package demoselenium1;
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Webdriverdemo {
+	public static void main(String[] args) {
+		WebDriver wd = new FirefoxDriver();
+		//wd.get("file:///C:/Selenium%20Software/Offline%20Website/index.html");
+		wd.get("file:///C:/Users/Shiv/Desktop/text%20document%20new/ABC.html");
+		
+		
+	}
+
+}
